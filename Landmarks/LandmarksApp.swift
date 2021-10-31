221b0a81158ed055a60b38cec2cbbbaae0b0e254
yuhara327 <51362368+Yuhara327@users.noreply.github.com>
2021-10-31 23:25:42 +0900

    Swift UI Tutorials

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,10 +1,3 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by 湯原壮一朗 on 2021/10/29.
-//
-
 import SwiftUI
 
 @main
---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            //マップ
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            //画像
+            CircleImage()
+                .offset(y :-130)
+                .padding(.bottom, -130)
+            //テキスト
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Tutle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
